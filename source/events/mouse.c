eead6936cdaf9b118c8fed225e3fb7fa1b9a3749
Louis <tagnacht2@gmail.com>
2025-08-16 21:48:33 +0200

    made mouse events free intermediate JSValues

--- source/events/mouse.c
+++ source/events/mouse.c
@@ -3,25 +3,6 @@
 
 extern JSValue rootValue;
 
-JSValue findValueById(JSContext *ctx, JSValue element, int searchKey)
-{
-    int key = GUI_GetKey(ctx, element);
-    if (key == searchKey)
-        return element;
-
-    JSValue children = GUI_GetChildren(ctx, element);
-    int num_children = GUI_GetLength(ctx, children);
-    for (int i = 0; i < num_children; i++)
-    {
-        JSValue child = JS_GetPropertyUint32(ctx, children, i);
-        JSValue foundValue = findValueById(ctx, child, searchKey);
-        if (!JS_IsUndefined(foundValue))
-            return foundValue;
-    }
-
-    return JS_UNDEFINED;
-}
-
 JSValue createMouseEvent(JSContext *ctx)
 {
     JSValue mouseEvent = JS_NewObject(ctx);
@@ -49,39 +30,29 @@ JSValue createMouseEvent(JSContext *ctx)
     return mouseEvent;
 }
 
-void EVENTS_InvokeMouseOver(JSContext *ctx, JSValue element)
-{
-    Vector2 delta = GetMouseDelta();
-    if (0 == delta.x + delta.y)
-        return;
-
-    JSValue props = JS_GetPropertyStr(ctx, element, "props");
-    if (JS_IsUndefined(props))
-        return;
-
-    JSValue mouseOverValue = JS_GetPropertyStr(ctx, props, "onMouseOver");
-    if (!JS_IsFunction(ctx, mouseOverValue))
-        return;
-
-    JSValue mouseEvent = createMouseEvent(ctx);
-    JS_Call(ctx, mouseOverValue, mouseOverValue, 1, &mouseEvent);
-}
-
 void EVENT_OnMouseOver(Clay_ElementId elementId, Clay_PointerData pointerInfo, intptr_t userData)
 {
     EventProps *eventProps = (EventProps *)userData;
     JSValue mouseOverFunc = JS_GetPropertyStr(eventProps->ctx, eventProps->props, "onMouseOver");
     if (!JS_IsFunction(eventProps->ctx, mouseOverFunc))
+    {
+        JS_FreeValue(eventProps->ctx, mouseOverFunc);
+        free(eventProps);
         return;
+    }
 
     JSValue event = createMouseEvent(eventProps->ctx);
+    JSValue ret = JS_Call(eventProps->ctx, mouseOverFunc, mouseOverFunc, 1, &event);
 
-    JS_Call(eventProps->ctx, mouseOverFunc, mouseOverFunc, 1, &event);
+    JS_FreeValue(eventProps->ctx, mouseOverFunc);
+    JS_FreeValue(eventProps->ctx, event);
+    JS_FreeValue(eventProps->ctx, ret);
 
+    JS_FreeValue(eventProps->ctx, eventProps->props);
     free(eventProps);
 }
 
-void EVENT_HandleMouseEvents(JSContext *ctx, JSValue element)
+void EVENT_HandleMouseEvents(JSContext *ctx, JSValueConst element)
 {
     JSValue props = JS_GetPropertyStr(ctx, element, "props");
     EventProps *eventProps = calloc(1, sizeof(EventProps));
