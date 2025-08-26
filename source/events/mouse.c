8c6d17f218093c656e9aeeab3484174bddb57dc0
Louis <tagnacht2@gmail.com>
2025-08-26 19:26:32 +0200

    added onMouseDown and onMouseUp

--- source/events/mouse.c
+++ source/events/mouse.c
@@ -30,7 +30,7 @@ JSValue createMouseEvent(JSContext *ctx)
     return mouseEvent;
 }
 
-void EVENT_OnMouseOver(JSContext *ctx, JSValueConst props)
+static void EVENT_OnMouseOver(JSContext *ctx, JSValueConst props)
 {
     JSValue mouseOverFunc = JS_GetPropertyStr(ctx, props, "onMouseOver");
     if (!JS_IsFunction(ctx, mouseOverFunc))
@@ -47,9 +47,71 @@ void EVENT_OnMouseOver(JSContext *ctx, JSValueConst props)
     JS_FreeValue(ctx, ret);
 }
 
+static void EVENT_OnMouseDown(JSContext *ctx, JSValueConst props)
+{
+    int pressedLeft = IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+    int pressedRight = IsMouseButtonPressed(MOUSE_BUTTON_RIGHT);
+    int pressedMiddle = IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE);
+    if (!pressedLeft && !pressedRight && !pressedMiddle)
+        return;
+
+    // Determines the number of the button pressed.
+    // For if right was pressed, the button is 1, middle is 2, and left remains 0.
+    int pressedButton = pressedLeft * 0 + pressedRight * 1 + pressedMiddle * 2;
+
+    JSValue clickFunc = JS_GetPropertyStr(ctx, props, "onMouseDown");
+    if (!JS_IsFunction(ctx, clickFunc))
+    {
+        JS_FreeValue(ctx, clickFunc);
+        return;
+    }
+
+    JSValue event = createMouseEvent(ctx);
+    // Set the button property on the event object.
+    JS_SetPropertyStr(ctx, event, "button", JS_NewInt32(ctx, pressedButton));
+
+    JSValue ret = JS_Call(ctx, clickFunc, clickFunc, 1, &event);
+
+    JS_FreeValue(ctx, clickFunc);
+    JS_FreeValue(ctx, event);
+    JS_FreeValue(ctx, ret);
+}
+
+static void EVENT_OnMouseUp(JSContext *ctx, JSValueConst props)
+{
+    int pressedLeft = IsMouseButtonReleased(MOUSE_BUTTON_LEFT);
+    int pressedRight = IsMouseButtonReleased(MOUSE_BUTTON_RIGHT);
+    int pressedMiddle = IsMouseButtonReleased(MOUSE_BUTTON_MIDDLE);
+    if (!pressedLeft && !pressedRight && !pressedMiddle)
+        return;
+
+    // Determines the number of the button pressed.
+    // For if right was pressed, the button is 1, middle is 2, and left remains 0.
+    int pressedButton = pressedLeft * 0 + pressedRight * 1 + pressedMiddle * 2;
+
+    JSValue clickFunc = JS_GetPropertyStr(ctx, props, "onMouseUp");
+    if (!JS_IsFunction(ctx, clickFunc))
+    {
+        JS_FreeValue(ctx, clickFunc);
+        return;
+    }
+
+    JSValue event = createMouseEvent(ctx);
+    // Set the button property on the event object.
+    JS_SetPropertyStr(ctx, event, "button", JS_NewInt32(ctx, pressedButton));
+
+    JSValue ret = JS_Call(ctx, clickFunc, clickFunc, 1, &event);
+
+    JS_FreeValue(ctx, clickFunc);
+    JS_FreeValue(ctx, event);
+    JS_FreeValue(ctx, ret);
+}
+
 void EVENT_OnHover(JSContext *ctx, JSValueConst props)
 {
     EVENT_OnMouseOver(ctx, props);
+    EVENT_OnMouseDown(ctx, props);
+    EVENT_OnMouseUp(ctx, props);
 }
 
 void EVENT_HandleMouseEvents(JSContext *ctx, JSValueConst element)
