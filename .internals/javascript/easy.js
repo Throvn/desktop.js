3438c3909f0a6edaa618cefc0fcc2f23da0c9cf4
Louis <tagnacht2@gmail.com>
2025-08-31 23:28:28 +0200

    fixed promises not resolving

--- .internals/javascript/easy.js
+++ .internals/javascript/easy.js
@@ -1,18 +1,2 @@
-import * as GUI from "GUI";
-
-class Test {
-
-    constructor(props) {
-        this.props = props;
-        this.i = 0;
-        setInterval(() => this.i++, 1000);
-    }
-    render = () => {
-        return GUI.createElement("text", null, "cool" + this.i);
-    }
-}
-const test = GUI.createElement(Test, null);
-console.log(test);
-console.log(JSON.stringify(GUI.createElement("test", null, "b")));
-
-GUI.render(test);
+const t = await fetch('https://example.com')
+console.log(await t.text())
